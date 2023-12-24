b284f560fcf68886515a49de52ed0d5666aa6112
yousefhajjar <yousef.hajjar.e22058@mcast.edu.mt>
2023-12-24 17:56:20 +0100

    Added Relationship model

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Car extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['model', 'year', 'salesperson_email'];
+
+    public function cars(){
+        return $this->hasMany(Manufacturer::class);
+    }
+
 }
